26a081c827512d38266df2aa46083865d5d60e97
Michael Schennum <mikeschenphoto@gmail.com>
2016-05-04 14:26:55 -0700

    adds xml pages

---
+++ app/src/main/java/com/example/guest/mymessages/models/User.java
@@ -0,0 +1,20 @@
+package com.example.guest.mymessages.models;
+
+/**
+ * Created by Guest on 5/4/16.
+ */
+public class User {
+    private String name;
+    private String location;
+
+    public User() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/example/guest/mymessages/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.guest.mymessages.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guest.mymessages.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
